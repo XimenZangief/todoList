d003e954187608bbe5eb224b9ff0efde640761f2
XimenZangief <s9671073@gmail.com>
2022-10-05 18:22:37 +0800

    feature: 增加建立時間

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js" integrity="sha512-jgqnY5wi2gxl4Yj4iXEEpVWrBI6JeJQc67LVC/a65X/TeMotFC0veqlMqYHAI3FfS3hCdx5z6fqKFzXqdnKXDw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body class="bg-info">
-    <div class="container w-25 mt-5">
+    <div class="container w-50 mt-5">
         <div class="card shadow-sm">
             <div class="card-body">
-                <h3>LARAVEL好難</h3>
+                <h2>LARAVEL好難</h2>
                 <form action="{{route('store')}}" method="post" autocomplete="off">
                     @csrf
                     <div class="input-group">
-                        <input type='test' name='content' class='form-control' placeholder="ADD"></input>
+                        <input type='test' name='content' class='form-control' placeholder="待辦事項"></input>
                         <button type="submit" class="btn btn-dark btn-sm px-4"><i class="fas fa-plus"></i></button>
                     </div>
                 </form>
@@ -29,6 +29,7 @@
                         @foreach ($todolists as $todolist)
                         <li class="list-group-item">
                             <form action="{{route('destroy',$todolist->id)}}" method="post">
+                                {{$todolist->created_at->format('Y-m-d')}}
                                 {{$todolist->content}}
                                 @csrf
                                 @method('delete')
@@ -41,7 +42,7 @@
                     <p class="text-center mt-3">沒東西啦</p>
                 @endif
                 <div class="card-footer">
-                    總共有 {{count($todolists)}} 個ADD。
+                    總共有 {{count($todolists)}} 個待辦事項。
                 </div>
             </div>
         </div>
